fcdae1a5b989f3a425d644ddcec97246f7a6397e
carolinepacker <sweetcaroline.packer@gmail.com>
2023-04-21 23:06:01 -0600

    Added Conditional Statements for Letter Grades.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Get User Input
+        Console.WriteLine("Please enter your grade percentage: ");
+        string userGrade = Console.ReadLine();
+        int grade = int.Parse(userGrade);
+
+        string letter = "";
+        // Feed it into conditional statement
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade < 90 && grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade < 80 && grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade < 70 && grade >= 60)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+        else
+        {
+            Console.Write("I'm sorry, there was a problem with your entry.");
+        }
+
+        Console.Write($"Your letter grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.Write("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.Write("I'm sorry, you did not pass the class. Best of luck next time!");
+        }
     }
 }
