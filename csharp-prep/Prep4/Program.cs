fbab58a9e80d5452841fa55d7caa284b2eb85f33
carolinepacker <sweetcaroline.packer@gmail.com>
2023-04-25 21:24:03 -0600

    Completed Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Adding to list
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int addNumber = -1;
+        List<int> numbers =  new List<int>();
+
+        while(addNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            addNumber = int.Parse(Console.ReadLine());
+
+            numbers.Add(addNumber);
+        }
+
+
+        // Calculations
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        double average = numbers.Average();
+        Console.WriteLine($"The average is: {average}");
+
+        int largest = numbers.Max();
+        Console.WriteLine($"The largest number is: {largest}");
+
     }
 }
