fabf5f0ac6fe93085f5b173d22df5a37655e0628
carolinepacker <sweetcaroline.packer@gmail.com>
2023-05-03 13:34:43 -0600

    Added Program file, error in file spelling last commit

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1  = new Job();
+        job1._company = "Apple";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2004;
+        job1._endYear = 2014;
+
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "CEO";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Caroline Nielson";
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
+
 }
