8da679ed77e2eff583a8fefa5cb7d51d2741117a
Caroline Packer <sweetcaroline.packer@gmail.com>
2023-06-01 17:36:34 -0600

    Started user experience

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        
+
+        //being menu loop
+        string choice = "-1";
+        while (choice != "4")
+        {
+            Console.WriteLine("Menu Options: ");
+            Console.WriteLine("1. Start breathing activity");
+            Console.WriteLine("1. Start reflecting activity");
+            Console.WriteLine("1. Start listing activity");
+            Console.WriteLine("4. Quit");
+            choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+                    
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
