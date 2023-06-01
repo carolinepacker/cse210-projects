0dc9d8d5bd715f53973540f9e8fd917c6123574d
Caroline Packer <sweetcaroline.packer@gmail.com>
2023-06-01 16:56:13 -0600

    Added all Assignment classes.

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,30 @@
+using System;
+
+public class Assignment
+{
+    // member variables
+    private string _studentName;
+    private string _topic;
+
+
+    public Assignment()
+    {
+
+    }
+
+    public Assignment(string name, string topic)
+    {
+        _studentName = name;
+        _topic = topic;
+    }
+
+    public string GetSummary()
+    {
+        return $"{_studentName} - {_topic}";
+    }
+
+    public string GetStudentName()
+    {
+        return _studentName;
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        WritingAssignment assignment1 = new WritingAssignment("Caroline Packer", "World History", "World War II");
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(assignment1.GetWritingInformation());
     }
 }
