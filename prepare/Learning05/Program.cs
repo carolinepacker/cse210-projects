89275a1c76fbc45850c4d44da1943b5b770350b6
carolinepacker <sweetcaroline.packer@gmail.com>
2023-06-13 19:02:59 -0600

    Added all shape classes and child classes.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square square1 = new Square("blue", 4);
+        Rectangle rect1 = new Rectangle("red", 3, 2);
+        Circle circle1 = new Circle("yellow", 2);
+        shapes.Add(square1);
+        shapes.Add(rect1);
+        shapes.Add(circle1);
+
+        foreach (Shape s in shapes) 
+        {
+            Console.WriteLine(s.GetColor());
+            Console.WriteLine(s.GetArea());
+            Console.WriteLine();
+        }
     }
 }
